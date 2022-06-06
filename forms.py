72701c0383dec5b89691cc812dcabf930c76fe09
Pradeep Rai <42033426+pradeeprai-82@users.noreply.github.com>
2022-06-06 08:01:17 +0530

    Add files via upload

---
+++ forms.py
@@ -0,0 +1,9 @@
+# from Flask_WTF import FlaskForm
+# from wtforms.validators import DataRequired, URL
+# from wtforms import StringField, SubmitField, PasswordField
+
+# from flask_ckeditor import CKEditorField
+
+# class CommentForm(FlaskForm):
+#     comment_text = CKEditorField("Comment", validators=[DataRequired()])
+#     submit = SubmitField("Submit Comment")
